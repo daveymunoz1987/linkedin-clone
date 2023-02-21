2e8b71e4139b5822638517fbf818c1629985e2a3
David Munoz <daveymunoz1987@gmail.com>
2023-02-20 19:58:46 -0500

    Half way through Feed, before starting Firebase

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -4,12 +4,12 @@ import { Avatar } from "@mui/material";
 
 function HeaderOption({ avatar, Icon, title}) {
   return (
-        <div className="headerOption">
+    <div className="headerOption">
         {Icon && <Icon className='headerOption_icon' />}
         {avatar && <Avatar className="headerOption_icon" src={avatar}/>} 
         
         <h3 className="headerOption_title">{title}</h3>
-  </div>
+    </div>
   );
 }
 
